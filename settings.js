401749f84aaf6d01bbf67ace7c5a4e6c1ca79961
Rxzyy-md <127937788+rxzyy-vl@users.noreply.github.com>
2024-08-25 20:04:17 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'Rulz'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "bkup206@gmail.com" //email
+global.email_password = "fdsx pwqy liaq kknh" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
